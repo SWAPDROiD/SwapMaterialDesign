dceb33a6a50da3e127d307d2ac490188a1eb3a21
DESKTOP-BAL1L5U\DELL <neha@convictionsoftwares.com>
2017-09-20 12:37:32 +0530

    @SwapnilNanadpure : Initial Commit.

---
+++ app/src/main/java/com/cs/demo/activities/FirstClass.java
@@ -0,0 +1,8 @@
+package com.cs.demo.activities;
+
+/**
+ * Created by raunakpatel on 17/08/16.
+ */
+
+public class FirstClass {
+}
---
+++ app/src/main/java/com/cs/demo/utils/Utils.java
@@ -0,0 +1,22 @@
+package com.cs.demo.utils;
+
+import android.content.Context;
+
+import com.cs.demo.R;
+
+/**
+ * Created by Suleiman on 30-04-2015.
+ */
+public class Utils {
+
+    public static int getToolbarHeight(Context context) {
+        int height = (int) context.getResources().getDimension(R.dimen.abc_action_bar_default_height_material);
+        return height;
+    }
+
+    public static int getStatusBarHeight(Context context) {
+        int height = (int) context.getResources().getDimension(R.dimen.statusbar_size);
+        return height;
+    }
+
+}
